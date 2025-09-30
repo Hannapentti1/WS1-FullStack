dd5d26dceb2666f2850172283149cb4c83722590
hannapentti <pentti.hanna@gmail.com>
2025-09-30 12:24:55 +0300

    task compeled

--- server.js
+++ server.js
@@ -2,8 +2,18 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World\\n');
+  response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  response.end(`
+    <!doctype html>
+    <html><head><meta charset="utf-8"><title>Taulukko</title></head>
+    <body>
+      <table border="1" style="border-collapse: collapse; width: 100%;">
+        <tr><th>Name</th><th>Address</th><th>City</th></tr>
+        <tr><td>Matti Meikäläinen</td><td>Timotie 1, as 10</td><td>Tampere</td></tr>
+        <tr><td>Maija Virtanen</td><td>Asematie 12</td><td>Kiljava</td></tr>
+      </table>
+    </body></html>
+  `);
 });
 
 server.listen(8081);
