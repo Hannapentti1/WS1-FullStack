b6f031417441f657ac384eb0310b046123fe44d5
hannapentti <pentti.hanna@gmail.com>
2025-09-30 12:06:17 +0300

    Completed workshop 1

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello World\\n');
+});
+
+server.listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
